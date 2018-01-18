91adc2bea97cb911c92ce818839902eacdc2ebc3
Blandine Mallat <blandinemallat@MacBook-Pro-de-Blandine.local>
2018-01-18 14:47:56 +0100

    Add of the dependency file for c sharp

---
+++ csharp.js
@@ -0,0 +1,36 @@
+/**
+ * Checkbox list examples
+ */
+
+'use strict';
+var inquirer = require('..');
+
+inquirer
+  .prompt([
+    {
+      type: 'checkbox',
+      message: 'Select c# section',
+      name: 'sections',
+      choices: [
+        new inquirer.Separator(' = available sections  = '),
+        {
+          name: 'Features'
+        },
+        {
+          name: 'Dependencies'
+        },
+        {
+          name: 'Philosophy'
+        }
+      ],
+      validate: function(answer) {
+        if (answer.length < 1) {
+          return 'You must choose at least one section.';
+        }
+        return true;
+      }
+    }
+  ])
+  .then(answers => {
+    console.log(JSON.stringify(answers, null, '  '));
+  });
